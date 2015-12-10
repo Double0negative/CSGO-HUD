494095e2c8bf25a24baabf5edb6687ecdbddc2b0
Double0negative <drew@puzlinc.com>
2015-12-10 02:39:08 -0500

    Format

--- server.js
+++ server.js
@@ -8,23 +8,23 @@
     app.set('view engine', 'jade');
 
     app.get('/', function(req, res) {
-    	res.render('index');
+        res.render('index');
     });
 
     app.get('/main.js', function(req, res) {
-    	res.sendfile('public/main.js');
+        res.sendfile('public/main.js');
     });
 
     app.get('/style.css', function(req, res) {
-    	res.sendfile('public/style.css');
+        res.sendfile('public/style.css');
     });
 
     io.on('connection', function(socket) {
-    	console.log('a user connected');
+        console.log('a user connected');
     });
 
     express.listen(2626, function() {
-    	console.log('listening on *:3030');
+        console.log('listening on *:3030');
     });
 
     port = 3000;
@@ -32,29 +32,29 @@
 
     server = http.createServer(function(req, res) {
 
-    	if (req.method == 'POST') {
-    		console.log("Handling POST request...");
-    		res.writeHead(200, {
-    			'Content-Type': 'text/html'
-    		});
+        if (req.method == 'POST') {
+            console.log("Handling POST request...");
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
 
-    		var body = '';
-    		req.on('data', function(data) {
-    			body += data;
-    		});
-    		req.on('end', function() {
-    			//console.log("POST payload: " + body);
-    			update(JSON.parse(body));
-    			res.end('');
-    		});
+            var body = '';
+            req.on('data', function(data) {
+                body += data;
+            });
+            req.on('end', function() {
+                //console.log("POST payload: " + body);
+                update(JSON.parse(body));
+                res.end('');
+            });
 
-    	} else {
-    		res.writeHead(200, {
-    			'Content-Type': 'text/html'
-    		});
-    		var html = 'yes';
-    		res.end(html);
-    	}
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+            var html = 'yes';
+            res.end(html);
+        }
 
     });
 
@@ -62,51 +62,50 @@
     var player;
 
     var round = {
-    	phase: "",
-    	timestart: 0,
-    	time: 0,
-    	bomb: {
-    		planted: false,
-    		timestart: 0,
-    		time: 0
-    	}
+        phase: "",
+        timestart: 0,
+        time: 0,
+        bomb: {
+            planted: false,
+            timestart: 0,
+            time: 0
+        }
     };
 
     function update(json) {
-    	if (json.round) {
-    		if (!(round.phase === json.round.phase)) {
-    			round.timestart = json.provider.timestamp;
-    			round.phase = json.round.phase;
-    		}
+        if (json.round) {
+            if (!(round.phase === json.round.phase)) {
+                round.timestart = json.provider.timestamp;
+                round.phase = json.round.phase;
+            }
 
-    		var maxTime = 0;
-    		if (json.round.phase === 'live') {
-    			maxTime = 115;
-    		} else if (round.phase === 'freezetime') {
-    			maxTime = 15;
-    		} else {
-    			maxTime = 7;
-    		}
-    		round.time = maxTime - (new Date().getTime() / 1000 - round.timestart);
+            var maxTime = 0;
+            if (json.round.phase === 'live') {
+                maxTime = 115;
+            } else if (round.phase === 'freezetime') {
+                maxTime = 15;
+            } else {
+                maxTime = 7;
+            }
+            round.time = maxTime - (new Date().getTime() / 1000 - round.timestart);
 
-            
             console.log(round.bomb.planted + " " + json.round.bomb + "  " + round.bomb.time);
-    		if (!round.bomb.planted && json.round.bomb === 'planted') {
-    			round.bomb.planted = true;
-    			round.bomb.timestart = json.provider.timestamp ;
-    		} else if (round.bomb.planted && json.round.bomb !== 'planted') {
-    			round.bomb.planted = false;
-    		} 
-            
-            if(round.bomb.planted) {
+            if (!round.bomb.planted && json.round.bomb === 'planted') {
+                round.bomb.planted = true;
+                round.bomb.timestart = json.provider.timestamp;
+            } else if (round.bomb.planted && json.round.bomb !== 'planted') {
+                round.bomb.planted = false;
+            }
+
+            if (round.bomb.planted) {
                 round.bomb.time = 40 - (new Date().getTime() / 1000 - round.bomb.timestart);
             }
 
-    		json.extra = {};
-    		json.extra.round = round;
-    	}
+            json.extra = {};
+            json.extra.round = round;
+        }
 
-    	io.emit("update", JSON.stringify(json));
+        io.emit("update", JSON.stringify(json));
     }
 
     server.listen(port, host);
